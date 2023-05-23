d005bd59cc52a21dcfd876afa5c32985ef8dc7ab
hamzic2019 <hamzicharis@icloud.com>
2023-05-23 19:25:59 +0200

    user class initial code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Test");
+        System.out.println("Bank Application");
+
+        User haris = new User("Haris Hamzic", "hamzicharis", 250);
+        User amina = new User("Amina Hamzic", "suljovicamina");
+        User esfija = new User("Esfija Hamzic", "hamzicesfija");
+
+
+        haris.sendMoney(amina, 49);
+        haris.sendMoney(amina, 25);
+        haris.sendMoney(esfija, 100);
+
+        haris.listTransactions();
+        amina.listTransactions();
+
+        haris.showBalance();
+        amina.showBalance();
+
     }
 }
---
+++ src/User.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private String name;
+    private String username;
+    private double balance;
+
+    private ArrayList<String> transactions = new ArrayList<>();
+    Date date = new Date();
+
+    public User(String name, String username, double balance){
+        this.name = name;
+        this.username = username;
+        this.balance = balance;
+    }
+
+    public User(String name, String username){
+        this.name = name;
+        this.username = username;
+        this.balance = 0.00;
+    }
+
+    public void showBalance(){
+        System.out.println(this.username + " current account balance amounts to: $" + this.balance);
+    }
+
+    public void sendMoney(User user2send, double amount){
+        this.balance -= amount;
+        user2send.balance += amount;
+        String message = ("On "+ date +",'"+ this.username +"' initiated a transfer of $"+ amount +" to recipient '"+user2send.username+"'");
+        transactions.add(message);
+
+        user2send.transactions.add(message);
+    }
+
+    public void listTransactions(){
+        System.out.println("- -- -");
+        int index = 1;
+        for(Object x : transactions){
+            System.out.println(index++ + ". " + x);
+        }
+    }
+
+    public void depositMoney(double amount){
+        this.balance += amount;
+    }
+
+}
